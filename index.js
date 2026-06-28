de447a7a2be2e44547e557b51fb410da02075da9
Prince-AA-4 <abigiboozine175@gmail.com>
2026-06-28 10:09:37 +0000

    cors setup notes

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// to link your frontend to your backend, you need to set up "Cors"
+//   run npm install cors and set it up like this
+//   app.use(
+//     cors({
+//       origin: Client url(the frontend url),
+//       credentials: true
+//   }));
+  
+// keep your frontend url in the .env and use the process() object to expose it like this
+// process.env.${environment variable}. Mostly, the client url is stored using CLIENT_URL.
+// But if you choose to use a different variable, you replace CLIENT_URL with that. it will be 
+// like process.env.CLIENT_URL
+
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
